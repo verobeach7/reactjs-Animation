03a63c8ff3a16faa294ff519d72d30fe569c8387
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-01-07 18:14:33 +0900

    React: inconvinient by repeating useState and function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from "styled-components";
+import ToDoList from "./ToDoList";
 
-const GlobalStyle = createGlobalStyle`@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
@@ -14,30 +16,30 @@ article, aside, canvas, details, embed,
 figure, figcaption, footer, header, hgroup,
 main, menu, nav, output, ruby, section, summary,
 time, mark, audio, video {
-margin: 0;
-padding: 0;
-border: 0;
-font-size: 100%;
-font: inherit;
-vertical-align: baseline;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
 }
 /* HTML5 display-role reset for older browsers */
 article, aside, details, figcaption, figure,
 footer, header, hgroup, main, menu, nav, section {
-display: block;
+  display: block;
 }
 /* HTML5 hidden-attribute fix for newer browsers */
 *[hidden] {
   display: none;
 }
 body {
-line-height: 1;
+  line-height: 1;
 }
 menu, ol, ul {
-list-style: none;
+  list-style: none;
 }
 blockquote, q {
-quotes: none;
+  quotes: none;
 }
 blockquote:before, blockquote:after,
 q:before, q:after {
@@ -45,27 +47,29 @@ content: '';
 content: none;
 }
 table {
-border-collapse: collapse;
-border-spacing: 0;
+  border-collapse: collapse;
+  border-spacing: 0;
 }
 * {
-box-sizing: border-box;
+  box-sizing: border-box;
 }
 body {
-font-family: 'Source Sans Pro', sans-serif;
-line-height: 1.2;
-
+  font-family: 'Source Sans Pro', sans-serif;
+  line-height: 1.2;
+  background-color:${(props) => props.theme.bgColor};
+  color:${(props) => props.theme.textColor};
 }
 a {
-text-decoration: none;
-color: inherit;
-}`;
+  text-decoration: none;
+  color: inherit;
+}
+`;
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <h1>Title</h1>
+      <ToDoList />
     </>
   );
 }
