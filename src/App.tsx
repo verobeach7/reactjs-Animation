7dff35280b0455e249f9e8e514b8e4fd376991e6
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-01-10 16:17:23 +0900

    React: Trello(Get Selectors)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
+
 function App() {
-  return <div></div>;
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector);
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // 앞에다가 +를 붙여주면 string을 number로 바꿔줌. +"1" -> 1
+    setMinutes(+event.currentTarget.value);
+  };
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input value={hours} type="number" placeholder="Hours" />
+    </div>
+  );
 }
 
 export default App;
