3ca2b91b57959f023a4d99544d9bbb69ec241683
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-01-11 21:49:02 +0900

    React-Animation: Variants Part1

--- src/App.tsx
+++ src/App.tsx
@@ -17,14 +17,16 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+// 두 개의 stage를 가짐. 이름은 상관 없음. initial/finish, start/end 등 상관 없음.
+const myVars = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box
-        transition={{ type: "spring", delay: 0.5 }}
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-      />
+      <Box variants={myVars} initial="start" animate="end" />
     </Wrapper>
   );
 }
