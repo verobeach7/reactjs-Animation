13bca068b08b1844ec7090a7c070722d18a75b84
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-01-12 18:43:45 +0900

    React-Animation: Layout

--- src/App.tsx
+++ src/App.tsx
@@ -13,72 +13,36 @@ const Wrapper = styled(motion.div)`
 
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   position: absolute;
   top: 100px;
   display: flex;
-  justify-content: center;
-  align-items: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const box = {
-  entry: (direction: number) => ({
-    x: direction > 0 ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 1,
-    },
-  },
-  exit: (direction: number) => ({
-    x: direction < 0 ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 1,
-    },
-  }),
-};
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  border-radius: 50px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 function App() {
-  const [[page, direction], setPage] = useState([1, 1]);
-  const paginate = (newDirection: number) => {
-    setPage([page + newDirection, newDirection]);
-  };
-  /* const [showing, setShowing] = useState(1);
-  const [back, setBack] = useState(false);
-  const nextShowing = () => {
-    setBack(false);
-    setShowing((prev) => (prev === 5 ? 5 : prev + 1));
-  };
-  const prevShowing = () => {
-    setBack(true);
-    setShowing((prev) => (prev === 1 ? 1 : prev - 1));
-  }; */
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
   return (
-    <Wrapper>
-      <AnimatePresence custom={direction}>
-        <Box
-          custom={direction}
-          variants={box}
-          initial="entry"
-          animate="center"
-          exit="exit"
-          key={page}
-        >
-          {page}
-        </Box>
-      </AnimatePresence>
-      <button onClick={() => paginate(-1)}>previous</button>
-      <button onClick={() => paginate(1)}>next</button>
+    <Wrapper onClick={toggleClicked}>
+      <Box
+        style={{
+          justifyContent: clicked ? "center" : "flex-start",
+          alignItems: clicked ? "center" : "flex-start",
+        }}
+      >
+        <Circle layout />
+      </Box>
     </Wrapper>
   );
 }
